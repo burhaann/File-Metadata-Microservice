a0aa6991b28f3a4e23d7c1ea2ceb42b1eac827b6
Burhaann <burhaann321@gmail.com>
2023-12-03 15:34:45 +0530

    USING MULTER

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
+const multer = require("multer");
 
 var app = express();
 
@@ -11,7 +12,17 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/", function (req, res) {});
+const upload = multer({ dest: "uploads/" });
+
+app.post(
+  "/api/fileanalyse",
+  upload.single("uploadedfile"),
+  function (req, res) {
+    const { originalname, mimetype, size } = req.file;
+    console.log(req.file);
+    res.json({ name: originalname, type: mimetype, size: size });
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
